23fb1550503ba75d24ead02c3fc09d189a7ea74c
rising lion <risinglion@ALGOKING.localdomain>
2025-09-24 12:57:04 +0300

    clear cache

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,12 @@ const app = express();
 console.log('Express app created');
 
 app.use(cors({
-  origin: ["https://fortune-farm-murex.vercel.app", "http://localhost:5173"],
+  origin: [
+    "https://fortune-farm-murex.vercel.app",
+    "https://fortune-farm-git-main-eliuds-projects-ebf8c589.vercel.app",
+    "https://fortune-farm-6ci3e05b5-eliuds-projects-ebf8c589.vercel.app",
+    "http://localhost:5173"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
